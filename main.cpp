3bc725032a4d16348b4c4435dc699909cf637161
Piboons <pauline.disalvocilia@gmail.com>
2018-10-23 14:30:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <iterator>
 
 using namespace std;
 string melangerMot(string mot)
@@ -18,42 +21,113 @@ string melangerMot(string mot)
     return motMelange;
 
 }
-
 int main()
 {
     srand(time(0));
     char quitter;
+    int nbJoueurs;
     do
     {
-        //1:Saisie du mot par J1
-        string motJ1;
-        cout << "Joueur 1, entrez un mot : " << endl;
-        cin >> motJ1;
-        cout << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl<< endl << endl << endl << endl << endl << endl << endl << endl << endl << endl;
-        //2:Mélange des lettres du mot
-        string motMelange(melangerMot(motJ1));
-        //3:On demande a J2 de trouver le mot
-        string motJ2;
-        int nbEssais(5);
+        cout << "Entrez le nombre de joueurs (1/2): ";
+        cin >> nbJoueurs;
+    }while (nbJoueurs != 1 && nbJoueurs != 2);
+
+    if (nbJoueurs == 2)
+    {
         do
         {
-            cout << "Joueur 2, vous avez " << nbEssais << " essai(s)." << endl;
-            cout << "Quel est ce mot ? " << motMelange << endl;
-            cin >> motJ2;
-            nbEssais -= 1;
-        }while (nbEssais > 0 && motJ2 != motJ1);
+            //1:Saisie du mot par J1
+            string motJ1;
+            cout << "Joueur 1, entrez un mot : " << endl;
+            cin >> motJ1;
+            cout << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl << endl<< endl << endl << endl << endl << endl << endl << endl << endl << endl << endl;
+            //2:Mélange des lettres du mot
+            string motMelange(melangerMot(motJ1));
+            //3:On demande a J2 de trouver le mot
+            string motJ2;
+            int nbEssais(5);
+            do
+            {
+                cout << "Joueur 2, vous avez " << nbEssais << " essai(s)." << endl;
+                cout << "Quel est ce mot ? " << motMelange << endl;
+                cin >> motJ2;
+                nbEssais -= 1;
+            }while (nbEssais > 0 && motJ2 != motJ1);
 
-        if (nbEssais > 0)
+            if (nbEssais > 0)
+            {
+                cout << "Bravo ! Voulez vous faire une autre partie ? (O/N)";
+                cin >> quitter;
+            }
+            else
+            {
+                cout << "Perdu ! Le mot etait " << motJ1 << ". Voulez vous faire une autre partie ? (O/N)" << endl;
+                cin >> quitter;
+            }
+
+
+        }while (quitter != 'n' && quitter != 'N');
+    }
+    else
+    {
+        //On importe le fichier dictionnaire
+        ifstream dico("dico.txt");
+        vector<string> dictionnaire;
+        istream_iterator<string> itFichierDico(dico);
+        istream_iterator<string> finDico;
+        back_insert_iterator<vector<string> > itDicoCopie(dictionnaire);
+        copy(itFichierDico, finDico, itDicoCopie);
+        vector<string>::iterator itDico;
+
+        if (dictionnaire.size()!=0)
         {
-            cout << "Bravo ! Voulez vous faire une autre partie ? (O/N)";
-            cin >> quitter;
+            do
+            {
+                //1: On choisit un mot aleatoirement dans le fichier dictionnaire
+                string motPC;
+                cout << "Generation du mot a deviner en cours, veuillez patienter..." << endl;
+
+                itDico = dictionnaire.begin();
+                int numLigne(rand() % 323577);
+                for (int k(0); k != numLigne; k++)
+                {
+                    itDico++;
+                }
+                motPC = *itDico;
+                //2: On melange les lettres du mot
+                string motMelange(melangerMot(motPC));
+                //3: On fait deviner le mot au joueur
+                string motJoueur;
+                int nbEssais(7);
+                do
+                {
+                    cout << "Vous avez " << nbEssais << " essai(s)." << endl;
+                    cout << "Quel est ce mot ? " << motMelange << endl;
+                    cin >> motJoueur;
+                    nbEssais -= 1;
+                }while (nbEssais > 0 && motPC != motJoueur);
+
+                if (nbEssais > 0)
+                {
+                    cout << "Bravo ! Voulez vous faire une autre partie ? (O/N)";
+                    cin >> quitter;
+                }
+                else
+                {
+                    cout << "Perdu ! Le mot etait " << motPC << ". Voulez vous faire une autre partie ? (O/N)" << endl;
+                    cin >> quitter;
+                }
+
+
+            }while (quitter != 'n' && quitter != 'N');
         }
         else
         {
-            cout << "Perdu ! Le mot etait " << motJ1 << ". Voulez vous faire une autre partie ? (O/N)" << endl;
-            cin >> quitter;
+            cout << "Erreur : impossible d'ouvrir le fichier dictionnaire." << endl;
         }
 
 
-    }while (quitter != 'n' && quitter != 'N');
+    }
+
+    return 0;
 }
